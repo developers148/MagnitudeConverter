a18a971276ae82d6b0a691bbee4f9248cf12ae2c
Michael Karmoker <Michaelshuvo@users.noreply.github.com>
2019-11-20 11:59:14 +0600

    Initial commit

---
+++ app/src/main/java/bd/com/rafi/magnitudeconverter/TitleActivity.java
@@ -0,0 +1,14 @@
+package bd.com.rafi.magnitudeconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
