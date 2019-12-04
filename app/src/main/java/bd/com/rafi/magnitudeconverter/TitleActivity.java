becee6c6f48e26f886e57f5b5873ae09e0ceb411
IshtiaqAhmadRion <43097671+IshtiaqAhmadRion@users.noreply.github.com>
2019-12-04 15:21:53 +0600

    number

--- app/src/main/java/bd/com/rafi/magnitudeconverter/TitleActivity.java
+++ app/src/main/java/bd/com/rafi/magnitudeconverter/TitleActivity.java
@@ -18,9 +18,9 @@ import android.widget.Toast;
 public class TitleActivity extends AppCompatActivity  implements View.OnClickListener {
 
     public String value1 = "Numeric", value2 = "Alphabetic";
-    ImageButton imageButton,imageButton2;
+    ImageButton imageButton1,imageButton2,imageButton3;
     RelativeLayout relativeLayout, relativeLayout2;
-    TextView textView1, textView2;
+    TextView textView1, textView2,textView3;
     Button submitBtn;
 
     @Override
@@ -30,24 +30,33 @@ public class TitleActivity extends AppCompatActivity  implements View.OnClickLis
 
         submitBtn = findViewById(R.id.submit);
 
-        imageButton = findViewById(R.id.imageBtn);
+        imageButton1 = findViewById(R.id.imageBtn1);
         textView1 = findViewById(R.id.textv1);
 
-        imageButton2 = findViewById(R.id.secimageBtn);
-        textView2 = findViewById(R.id.sectextv1);
+        imageButton2 = findViewById(R.id.imageBtn2);
+        textView2 = findViewById(R.id.textv2);
 
-        imageButton.setOnClickListener(this);
+        imageButton3 = findViewById(R.id.imageBtn3);
+        textView3 = findViewById(R.id.textv3);
+
+        imageButton1.setOnClickListener(this);
         textView1.setOnClickListener(this);
 
         imageButton2.setOnClickListener(this);
         textView2.setOnClickListener(this);
+
+        imageButton3.setOnClickListener(this);
+        textView3.setOnClickListener(this);
+
+
+
         submitBtn.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.imageBtn || view.getId() == R.id.textv1) {
+        if (view.getId() == R.id.imageBtn1 || view.getId() == R.id.textv1) {
             showPopup(view);
 
         }
@@ -69,17 +78,54 @@ public class TitleActivity extends AppCompatActivity  implements View.OnClickLis
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
                 switch (menuItem.getItemId()){
-                    case R.id.menuitem1:
-                        textView1.setText("Numeric");
+                    case R.id.ConvertDollarToBDT:
+                        textView1.setText("Convert Dollar To BDT");
                         value1 = textView1.getText().toString();
-                        Log.e("checking",value1);
-                        textView2.setText("Alphabetic");
+
+                        //textView2.setText("Alphabetic");
+
+                        if(value1 == "Convert Dollar To BDT"){
+
+                        }
                         return true;
-                    case R.id.menuitem2:
-                        textView1.setText("Alphabetic");
+
+                    case R.id.LengthConverter:
+                        textView1.setText("Length Converter");
                         value1 = textView1.getText().toString();
-                        Log.e("checking",value1);
-                        textView2.setText("Numeric");
+                        //textView2.setText("Numeric");
+                        return true;
+
+                    case R.id.NumberConverter:
+                        textView1.setText("Number Converter");
+                        value1 = textView1.getText().toString();
+                        //textView2.setText("Numeric");
+                        return true;
+
+                    case R.id.NumbersToWord:
+                        textView1.setText("Numbers To Word");
+                        value1 = textView1.getText().toString();
+                        //textView2.setText("Numeric");
+                        return true;
+
+                    case R.id.pressureConverter:
+                        textView1.setText("Pressure Converter");
+                        value1 = textView1.getText().toString();
+                        //textView2.setText("Numeric");
+                        return true;
+                    case R.id.timeConverter:
+                        textView1.setText("Time Converter");
+                        value1 = textView1.getText().toString();
+                        //textView2.setText("Numeric");
+                        return true;
+                    case R.id.weightMassConverter:
+                        textView1.setText("Weight Mass Converter");
+                        value1 = textView1.getText().toString();
+                        //textView2.setText("Numeric");
+                        return true;
+                    case R.id.wordToNumber:
+                        textView1.setText("Word To Number");
+                        value1 = textView1.getText().toString();
+                        //textView2.setText("Numeric");
                         return true;
 
                     default:
